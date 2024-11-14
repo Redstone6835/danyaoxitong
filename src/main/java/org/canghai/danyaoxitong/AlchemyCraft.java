84c3d40a960f0524bdff6866666261d3ff4b6aa1
CanghaiCC <haicang666@gmail.com>
2024-11-14 18:55:29 +0800

    修复合成控件的逻辑问题，待修复：合成整体逻辑

--- src/main/java/org/canghai/danyaoxitong/AlchemyCraft.java
+++ src/main/java/org/canghai/danyaoxitong/AlchemyCraft.java
@@ -7,16 +7,10 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.canghai.danyaoxitong.items.herbs.HerbType;
 import org.canghai.danyaoxitong.items.pills.PillType;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class AlchemyCraft {
-    private int level = 0; //  品质等级 1-3
-    private String lastLore = "";
     private final Random random = new Random(); //  用来随机生成丹药
 
     public AlchemyCraft() {
@@ -29,44 +23,52 @@ public class AlchemyCraft {
     */
 
     public int checkRecipe(List<List<String>> ingredients, Player player) {
+        int level = 0; //  品质等级 1-3
         int size = ingredients.size();
-        for (List<String> innerList : ingredients) {
+        String lastLore = "";
+        if(size == 5) {
+            for (List<String> innerList : ingredients) {
+                String firstLore = innerList.get(0);//  获取药草品质数据
 
-            String firstLore = innerList.get(0);
-            //  调试
-            player.sendMessage(firstLore);
-            if (lastLore != "" && firstLore != lastLore) {
-                player.sendMessage("请输入相同品质的物品物品");
-                return 0;
+                //  调试
+                player.sendMessage(firstLore);
+
+                    if (lastLore != "" && firstLore != lastLore) {
+                        player.sendMessage("请输入相同品质的药草");
+                        return 0;
+                    }
+
+                    lastLore = firstLore;
+
+                    switch (firstLore) {
+                        case "上品药草":
+                            level = 1;
+                            break;
+                        case "仙品药草":
+                            level = 2;
+                            break;
+                        case "稀世药草":
+                            level = 3;
+                            break;
+                    }
             }
-            lastLore = firstLore;
-            if(size == 5) {
-                switch (firstLore) {
-                    case "上品药草":
-                        level = 1;
-                        break;
-                    case "仙品药草":
-                        level = 2;
-                        break;
-                    case "稀世药草":
-                        level = 3;
-                        break;
-                }
-            }
+        } else {
+            player.sendMessage("请放入5个同品质药草！");
         }
+
         return level;
     }
 
     // 根据配方生成对应的丹药物品
     public ItemStack createPill(int pillLevel) {
         if (pillLevel != 0) {
-            ItemStack pillItem = new ItemStack(Material.POTION);//  丹药材质待定
-            ItemMeta meta = pillItem.getItemMeta();
-
             List<ItemStack> possiblePills = new ArrayList<>();
             //  遍历丹药数据并将合成结果对应品质的丹药放到静态集合里
+
             for (Map.Entry<String, PillType> entry : PillType.getLoadedPills().entrySet()) {
                 PillType pill = entry.getValue();
+                ItemStack pillItem = new ItemStack(pill.getMaterial());//  丹药材质待定
+                pillItem.setLore(Arrays.asList(pill.getDescription()));
 
                 if (pill.getQuality() == pillLevel) {
                     possiblePills.add(pillItem);
